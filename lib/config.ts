751f8fff9c22bd1d9656f930b834efa5df7b7c30
marwanfathy <100175498+marwanfathy@users.noreply.github.com>
2026-04-21 17:29:19 +0200

    Update API and MEDIA URLs to ngrok endpoints

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://192.168.1.100:5001';
-export const MEDIA_URL = process.env.NEXT_PUBLIC_MEDIA_URL || 'http://192.168.1.100:5002';
+export const API_URL = 'https://0f7e-197-54-41-226.ngrok-free.app';
+export const MEDIA_URL ='https://a0b9-197-54-41-226.ngrok-free.app';
