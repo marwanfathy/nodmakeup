601df6a4a10d28acff6cbd494a4719bc421efec3
marwanfathy <100175498+marwanfathy@users.noreply.github.com>
2026-04-21 16:41:44 +0200

    Update API_URL and MEDIA_URL to new endpoints

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://57a6-197-54-41-226.ngrok-free.app';
-export const MEDIA_URL ='https://0243-197-54-41-226.ngrok-free.app';
+export const API_URL = 'https://0f7e-197-54-41-226.ngrok-free.app';
+export const MEDIA_URL ='https://a0b9-197-54-41-226.ngrok-free.app';
