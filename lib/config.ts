e25f5b21830250295ee284d037ef030e9233d43d
marwanfathy <100175498+marwanfathy@users.noreply.github.com>
2026-06-19 15:59:12 +0300

    Update MEDIA_URL to new ngrok address

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://6956-105-36-96-27.ngrok-free.app';
-export const MEDIA_URL ='https://7eff-197-54-41-226.ngrok-free.app';
+export const MEDIA_URL ='https://f22d-105-36-96-27.ngrok-free.app';
