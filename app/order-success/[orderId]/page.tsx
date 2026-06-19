af802c141b8d3151e2fdc1143aea45692cba4fb7
marwanfathy <100175498+marwanfathy@users.noreply.github.com>
2026-06-19 16:15:40 +0300

    Update page.tsx

--- app/order-success/[orderId]/page.tsx
+++ app/order-success/[orderId]/page.tsx
@@ -8,7 +8,7 @@ import { getPublicOrderDetails, OrderDetails } from '@/lib/api';
 import Spinner from '@/app/Common/spinn'; 
 // @ts-ignore
 import './OrderSuccess.css';
-const audiosrc = "http://192.168.1.18:5002/uploads/audio/successfx.mp3";
+const audiosrc = "https://f22d-105-36-96-27.ngrok-free.app/uploads/audio/successfx.mp3";
 
 export default function OrderSuccessPage() {
     const params = useParams();
