a6b18d3ca8646a042406e3ca27ccf21219f63c8e
marwanfathy <100175498+marwanfathy@users.noreply.github.com>
2026-06-29 22:10:21 +0300

    Update API and MEDIA URLs in config

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://6956-105-36-96-27.ngrok-free.app';
-export const MEDIA_URL ='https://f22d-105-36-96-27.ngrok-free.app';
+export const API_URL = 'https://392e-197-54-67-24.ngrok-free.app';
+export const MEDIA_URL ='https://ea2f-197-54-67-24.ngrok-free.app';
